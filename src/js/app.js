f6c12b883effaeba31bbe931128e472d74291030
Evgeny <salabaikin@mail.ru>
2021-01-14 01:06:55 +0300

    Add footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Code
+  var promoSlider = new Swiper('.promo-slider.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var compositeSlider = new Swiper('.composite-slider.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    spaceBetween: 30,
+  });
+  var monoSlider = new Swiper('.mono-slider.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    spaceBetween: 30,
+  });
 
 });
 
