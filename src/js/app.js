13351eab4c2d30355a476016d9683c2f87f0f96d
Evgeny <salabaikin@mail.ru>
2021-01-17 23:19:32 +0300

    Add order

--- src/js/app.js
+++ src/js/app.js
@@ -160,4 +160,40 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+  // Tabs
+  class Tabs {
+    constructor(button, content) {
+      this.button = button;
+      this.content = content;
+    }
+    render() {
+      const showTabs = (el) => {
+        const btnTarget = el.currentTarget;
+        const count = btnTarget.dataset.count;
+        this.content.forEach((el) => {
+          this.removeClass(el);
+        });
+        this.button.forEach((el) => {
+          this.removeClass(el);
+        });
+        document.querySelector('#' + count).classList.add('active');
+        btnTarget.classList.add('active');
+      }
+      this.button.forEach(function(el) {
+        el.addEventListener("click", showTabs);
+      });
+    }
+    removeClass(el) {
+      el.classList.remove('active');
+    }
+  }
+
+  // Delivery tabs
+  const deliveryTabLinks = document.querySelectorAll('.order-delivery-link'),
+        deliveryTabContent = document.querySelectorAll('.order-delivery-content');
+  if (deliveryTabLinks && deliveryTabContent) {
+    const deliveryTabs = new Tabs(deliveryTabLinks, deliveryTabContent);
+    deliveryTabs.render();
+  }
+
 });
